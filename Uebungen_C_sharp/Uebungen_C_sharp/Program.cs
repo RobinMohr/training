872713ba422d818524ca2110dd28bf2ecd017cc0
Mohr, Robin <robin.mohr@tecalliance.net>
2022-09-15 11:41:11 +0200

    printing out the sum of two numbers

---
+++ Uebungen_C_sharp/Uebungen_C_sharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebungen_C_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 5;
+            int number2 = 6;
+            int number = number1 + number2;
+
+            Console.WriteLine(number);
+
+
+            Console.ReadLine();
+        }
+    }
+}
