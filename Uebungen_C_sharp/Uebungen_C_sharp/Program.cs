a516b5f49ac006c8b713c29214684cfb81a50ba1
Mohr, Robin <robin.mohr@tecalliance.net>
2022-09-15 13:58:20 +0200

    filling a text from the user with y's from the right until it has 30 characters trimming a text so the spaces in front and at the end of it get deleted

--- Uebungen_C_sharp/Uebungen_C_sharp/Program.cs
+++ Uebungen_C_sharp/Uebungen_C_sharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -40,13 +41,26 @@ namespace Uebungen_C_sharp
 
             int words = entries.Length;
             Console.WriteLine("Dein Text hat " + words + " Wörter."); 
-            */
+            
 
             //Text von links bis zu 20 stellen mit y's auffüllen
 
             Console.WriteLine("Gebe bitte einen Text ein:");
             string text5 = Console.ReadLine();
             Console.WriteLine(text5.PadLeft(20, 'y'));
+            
+
+            //Text von rechts bis zu 30 stellen mit y's auffüllen
+            Console.WriteLine("Gebe bitte einen Text ein:");
+            string text6 = Console.ReadLine();
+            Console.WriteLine(text6.PadRight(30, 'y'));
+            */
+
+            //
+            Console.WriteLine("Gebe bitte einen Text ein mit ein paar Leerzeichen vor und nach dem Text!");
+            string text7 = Console.ReadLine();
+            Console.WriteLine(text7.Trim());
+
 
 
 
