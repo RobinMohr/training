8dfdb4af985735de41db144946fad99819ee9ebd
Mohr, Robin <robin.mohr@tecalliance.net>
2022-09-15 11:49:12 +0200

    user can send in a text, all U,u will change to G,g

--- Uebungen_C_sharp/Uebungen_C_sharp/Program.cs
+++ Uebungen_C_sharp/Uebungen_C_sharp/Program.cs
@@ -10,13 +10,28 @@ namespace Uebungen_C_sharp
     {
         static void Main(string[] args)
         {
+            //zwei zahlen addieren
             Console.WriteLine("Bitte gib die erste Zahl ein:");
             int number1 = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Bitte gib die zweite Zahl ein:");
             int number2 = Convert.ToInt32(Console.ReadLine());
+
             int number = number1 + number2;
 
             Console.WriteLine(number);
+
+            //im text u,U mit g,G ersetzen
+            Console.WriteLine("Bitte gebe einen Text ein:");
+            string entry = Console.ReadLine();
+
+            string replaced_entry = entry.Replace("U", "G").Replace("u", "g");
+
+            Console.WriteLine("Du hast geschrieben: " + entry);
+            Console.WriteLine("Ich habe daraus gemacht: " + replaced_entry);
+
+            
+
 
 
             Console.ReadLine();
