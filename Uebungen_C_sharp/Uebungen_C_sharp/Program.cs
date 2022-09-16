99ca7666b97454c01be356dcae2f838d24b99a3a
Mohr, Robin <robin.mohr@tecalliance.net>
2022-09-16 09:15:10 +0200

    arrays in gerneral and the average, minimal and maximal

--- Uebungen_C_sharp/Uebungen_C_sharp/Program.cs
+++ Uebungen_C_sharp/Uebungen_C_sharp/Program.cs
@@ -101,15 +101,158 @@ namespace Uebungen_C_sharp
                 sum = sum + j;
                 Console.WriteLine(sum);
             }
-            */
+            
 
-            //
+            //von bestimmter zahl runterzählen
             int j = Convert.ToInt32(Console.ReadLine());
             
             for (int i = 0; j > i; j--)
             {
                 Console.WriteLine(j);
             } 
+            
+
+            //
+            Console.WriteLine("gib einen buchstaben ein nachdem du suchen willst");
+            string input = Console.ReadLine();
+
+
+            var namen = new List<string> { "Hannes", "Marcello", "Robert", "Erwin", "Jonas", "Robin"};
+            foreach (var element in namen)
+            {
+                if (element.Contains(input))
+                {
+                    Console.Write($"{element} ");
+                }
+            }   
+            
+
+            Random random = new Random();
+            int random_zahl = random.Next(0, 100);
+            
+            while (true){
+                Console.WriteLine("Rate eine Zahl zwischen 0 und 100");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if(guess == random_zahl)
+                {
+                    Console.WriteLine("Du hast gewonnen!");
+                    break;
+                }
+                else if(guess < random_zahl)
+                {
+                    Console.WriteLine("Die zufällige Zahl ist größer.");
+                }
+                else if(guess > random_zahl)
+                {
+                    Console.WriteLine("Die zufällige Zahl ist kleiner.");
+                }
+            }
+            Console.WriteLine("Willst du nochmal spielen?");
+            string again = Console.ReadLine();
+
+            Random random1 = new Random();
+            int random_zahl1 = random1.Next(0, 100);
+
+            if (again.Contains("j")){
+
+                Console.WriteLine("Rate eine Zahl zwischen 0 und 100");
+                int guess1 = 0;
+                do
+                {
+
+                    int guess1 = Convert.ToInt32(Console.ReadLine());
+
+                    if (guess1 == random_zahl1)
+                    {
+                        Console.WriteLine("Du hast gewonnen!");
+                        break;
+                    }
+                    else if (guess1 < random_zahl1)
+                    {
+                        Console.WriteLine("Die zufällige Zahl ist größer.");
+                    }
+                    else if (guess1 > random_zahl1)
+                    {
+                        Console.WriteLine("Die zufällige Zahl ist kleiner.");
+                    }
+                } while (guess1 != random_zahl1);
+            }
+            
+
+            int[] zahlen = new int[5];
+
+            for(int i = 0; i< zahlen.Length; i++)
+            {
+                Console.WriteLine("gebe mir eine Zahl für das Array!");
+                zahlen[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            for(int i = 0; i< zahlen.Length; i++)
+            {
+                Console.WriteLine($"|  {zahlen[i]}  |");
+            }
+            
+
+            string[] namen = new string[13];
+            string[] namen2 = new string[] 
+            {
+                "Lukas", "Jonas", "Mona", "Marie", 
+                "Phillip", "Jan", "Erwin", "Leon", 
+                "Marcello", "Johannes", "Robin", 
+                "Andreas", "Robert"
+            };
+
+            foreach(string trainee in namen2)
+            {
+                Console.WriteLine(trainee);  
+            }
+
+            for(int i = 0; i< namen2.Length; i++)
+            {
+                namen2[i] += " 231";
+                Console.WriteLine(namen2[i]);
+            }
+            */
+
+            //array (aufgabe 4)
+
+            Console.WriteLine("Wie viele Zahlen soll die tabelle haben?");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            int[] zahlen = new int[i];
+
+            int t = 1;
+
+            for (int j = 0; j < i; j++)
+            { 
+                Console.WriteLine("Bitte gebe nun die " + t + ". Zahl ein.");
+                zahlen[j] = Convert.ToInt32(Console.ReadLine());
+                t = t + 1;
+            }
+            Console.Clear();
+            foreach(int oi in zahlen)
+            {
+                Console.WriteLine(oi);
+            }
+
+            double min = zahlen.Min();
+            Console.WriteLine("Der nniedrigste Wert ist " + min + ".");
+
+            double max = zahlen.Max();
+            Console.WriteLine("Der höchste Wert ist " + max + ".");
+
+            double avg = zahlen.Average();
+            Console.WriteLine("Der Durchschnittswert ist " + avg + ".");
+
+
+
+
+
+
+
+
 
 
             Console.ReadLine();
